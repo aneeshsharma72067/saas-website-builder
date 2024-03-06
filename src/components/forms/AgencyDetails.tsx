7886aab5dbd22252acd5262594a8937c45d5237d
Aneesh Sharma <aneeshsharma1024@gmail.com>
2024-03-06 22:23:24 +0530

    Created Agency Details Page and Agency Form Page

---
+++ src/components/forms/AgencyDetails.tsx
@@ -0,0 +1,103 @@
+"use client";
+
+import { Agency } from "@prisma/client";
+import React, { useEffect, useState } from "react";
+import { useToast } from "../ui/use-toast";
+import { useRouter } from "next/navigation";
+import { AlertDialog } from "../ui/alert-dialog";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
+import { Form, FormControl, FormField, FormItem, FormLabel } from "../ui/form";
+import { useForm } from "react-hook-form";
+
+import * as Z from "zod";
+
+type Props = {
+  data?: Partial<Agency>;
+};
+
+const FormSchema = Z.object({
+  name: Z.string().min(2, { message: "Agency Name must be at least 2 chars." }),
+  companyEmail: Z.string().min(1),
+  companyPhone: Z.string().min(1),
+  whiteLabel: Z.boolean(),
+  address: Z.string().min(1),
+  city: Z.string().min(1),
+  zipCode: Z.string().min(1),
+  state: Z.string().min(1),
+  country: Z.string().min(1),
+  agencyLogo: Z.string().min(1),
+});
+
+const AgencyDetails = ({ data }: Props) => {
+  const { toast } = useToast();
+  const router = useRouter();
+  const [deletingAgency, setDeletingAgency] = useState(false);
+
+  const form = useForm<Z.infer<typeof FormSchema>>({
+    mode: "onChange",
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      name: data?.name,
+      companyEmail: data?.companyEmail,
+      companyPhone: data?.companyPhone,
+      whiteLabel: data?.whiteLabel || false,
+      address: data?.address,
+      city: data?.city,
+      zipCode: data?.zipCode,
+      state: data?.state,
+      country: data?.country,
+      agencyLogo: data?.agencyLogo,
+    },
+  });
+  const isLoading = form.formState.isSubmitting;
+  useEffect(() => {
+    if (data) {
+      form.reset(data);
+    }
+  }, [data]);
+
+  const handleSubmit = async () => {};
+
+  return (
+    <AlertDialog>
+      <Card className="w-full">
+        <CardHeader>
+          <CardTitle>Agency Information</CardTitle>
+          <CardDescription>
+            Let's create an agency for your business. You can edit agency
+            settings later from the agency settings tab.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form
+              className="space-y-4"
+              onSubmit={form.handleSubmit(handleSubmit)}
+            >
+              <FormField
+                disabled={isLoading}
+                control={form.control}
+                name="agencyLogo"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Agency Logo</FormLabel>
+                    <FormControl></FormControl>
+                  </FormItem>
+                )}
+              ></FormField>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </AlertDialog>
+  );
+};
+
+export default AgencyDetails;
