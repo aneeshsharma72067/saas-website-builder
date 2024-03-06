f16dc15e07443070b7a26861a480fb65dc49c6f1
Aneesh Sharma <aneeshsharma1024@gmail.com>
2024-03-06 18:52:30 +0530

    User Agency Login

--- src/app/(main)/agency/page.tsx
+++ src/app/(main)/agency/page.tsx
@@ -4,10 +4,9 @@ import { redirect } from "next/navigation";
 import React from "react";
 
 const Page = async () => {
-  const authUser = await currentUser();
-  if (!authUser) return redirect("/sign-in");
-
   const agencyId = await verifyAndAcceptInvitation();
+  console.log(agencyId);
+
   // get user details
   const user = await getAuthUserDetails();
 
